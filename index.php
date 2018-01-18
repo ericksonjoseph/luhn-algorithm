d0ec4ccf3dee3d93f04096fe288460d6150a95e9
Erickson Joseph <erickson1.joseph@gmail.com>
2018-01-17 23:39:55 -0500

    Handle invalid CLI usage

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 namespace EricksonJoseph;
 
+if (!isset($argv[1])) {
+    echo "usage: php index.php 79927398713\n";
+    exit(2);
+}
+
 require_once('./vendor/autoload.php');
 
 $input = $argv[1];
